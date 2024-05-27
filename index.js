fb740693624666cbecc84cc0052b3ffdd39c5ebf
ilyTanu <tanmayabhyankar987@gmail.com>
2024-05-27 22:39:24 +0530

    changes

---
+++ index.js
@@ -0,0 +1,15 @@
+document
+  .getElementById("secretForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const secretCode = document.getElementById("secretCode").value;
+    const correctCode = "TT25062308";
+    const errorMessage = document.getElementById("errorMessage");
+
+    if (secretCode === correctCode) {
+      window.location.href = "home.html";
+    } else {
+      errorMessage.textContent = "Arey Cutu ye galat code hai";
+    }
+  });
